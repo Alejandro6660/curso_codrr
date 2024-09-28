05af847827d7e71432ae2ca1136fdf038e5a0f6a
Alex <alejandro.fuentesgzm@gmail.com>
2024-09-28 12:40:18 -0600

    [ADD]-config center of develop

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from '@/app.module';
+import { ConfigService } from '@nestjs/config';
+import * as morgan from 'morgan';
+import { CORS } from '@/constants/cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.use(morgan('dev'));
+
+  const configServcie = app.get(ConfigService);
+
+  app.setGlobalPrefix('api');
+
+  app.enableCors(CORS);
+
+  await app.listen(configServcie.get('PORT'));
+
+  console.log(`aplication runnning on: ${await app.getUrl()}`);
 }
 bootstrap();
